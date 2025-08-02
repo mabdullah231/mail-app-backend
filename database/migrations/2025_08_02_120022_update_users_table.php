13cda5a8a3e5581fff319199613c4a4a568a0e8f
mabdullah231 <69899498+mabdullah231@users.noreply.github.com>
2025-08-02 12:16:14 +0500

    Basic Sanctum Backend

---
+++ database/migrations/2025_08_02_120022_update_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->enum('user_type', [0, 1, 2])->default(2)->comment('0: Admin, 1: Company, 2: User');
+            $table->unsignedInteger('code')->nullable()->comment('Registration verification code');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['user_type', 'code', 'deleted_at']);
+        });
+    }
+};
